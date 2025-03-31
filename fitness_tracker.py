be947e8362ae311546e3522e877ff6b5da0c5416
Alen-T-Alex <140389582+Alen-T-Alex@users.noreply.github.com>
2025-04-01 01:03:25 +0300

    Add files via upload

---
+++ fitness_tracker.py
@@ -0,0 +1,5 @@
+number_of_steps = 10000
+distance = 8.1
+goal_steps = 10000
+if_goal_is_met = True
+print(f"hello my name is alen, my step goal on my watch is {number_of_steps} and distance is {distance}. goal reached? {if_goal_is_met}")
